003ab6a29c4dc21e9b9c4e03c0aa865a48351e78
semkud <semeon1998@gmail.com>
2023-01-16 16:09:00 +0300

    upload

---
+++ data_miner.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+#parsing table with cities
+url = 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%D0%BE%D0%B2_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8'
+page = requests.get(url)
+soup = BeautifulSoup(page.text, "html.parser")
+allCities = soup.findAll('tr')
+s = []
+for city in range(2,1119):
+  c = allCities[city].findAll('td')
+  if city % 10 == 0:
+    print(city*100/1119)
+  try:
+    href = "https://ru.wikipedia.org"+c[2].find('a').get('href')
+    page = requests.get(href)
+    soup = BeautifulSoup(page.text, "html.parser")
+    short = soup.findAll('p')
+    sh = short[0].text
+  except:
+    sh = 'Простите, про этот город у нас нет подробной информации!'
+  zx = c[2].text
+  z = c[2].text.lower()
+  if zx.endswith('не призн.'):
+    zx = zx[:-9]
+    z = z[:-9]
+  f_letter = z[0]
+  if z.endswith('ый'):
+    l_letter = z[-3]
+  elif z[-1] not in ['ь', 'ы', 'й']:
+    l_letter = z[-1]
+  else:
+    l_letter = z[-2]
+  s.append([int(c[0].text), z, zx, c[3].text, int(c[5].text.replace(" ", "")), sh, f_letter, l_letter])
+
+mydf = pd.DataFrame(s, columns = ['number', 'city', 'City', 'region', 'population', 'description', 'first_letter', 'last_letter'])
+
+mydf.to_csv('cities.csv', sep = '\t')
---
+++ main.py
@@ -0,0 +1,135 @@
+import telebot
+from telebot import types
+import conf
+import random
+import pandas as pd
+import copy
+
+df_base = pd.read_csv('cities.csv', sep='\t')
+df_base['played'] = 0
+
+bot = telebot.TeleBot(conf.TOKEN)
+
+users = {}
+default_settings = {'game_status': 0, 'difficult': 1, 'threshold': 500000, 'key_letter': '', 'df': copy.deepcopy(df_base)}
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    user_id = message.chat.id
+    bot.send_message(user_id, "Привет! Этот бот знает все города России* и может сыграть с вами в них. Чтобы начать игру - отправьте /startgame. Если вы хотите узнать больше информации о городе - перешлите название города с командой /info. В начале игры вам надо будет выбрать сложность. Чем выше сложность - тем больше городов будет знать бот. Удачи! :)\n\n\n\n\n\n*Имеются в виду города, указанные на соответствующей странице Википедии, бот не оспаривает территориальную целостность России")
+
+
+@bot.message_handler(commands=['info'])
+def send_info(message):
+    user_id = message.chat.id
+    try:
+        city = message.reply_to_message.text.lower()
+        if is_city(bot, city, user_id):
+            info = df_base[df_base['city'] == city]['description'].tolist()[0]
+            bot.send_message(user_id, info)
+    except AttributeError:
+        bot.send_message(user_id, 'Перешлите название города, информацию о котором, хотите узнать')
+
+
+@bot.message_handler(commands=['startgame'])
+def send_welcome(message):
+    user_id = message.chat.id
+    users[user_id] = copy.deepcopy(default_settings)
+    keyboard = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton(text="Просто", callback_data="button1")
+    button2 = types.InlineKeyboardButton(text="Нормально", callback_data="button2")
+    button3 = types.InlineKeyboardButton(text="Сложно", callback_data="button3")
+    keyboard.add(button1)
+    keyboard.add(button2)
+    keyboard.add(button3)
+    bot.send_message(message.chat.id, "Выберите, пожалуйста, сложность игры", reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    user_id = call.message.chat.id
+    if call.message:
+        users[user_id]['game_status'] = 1
+        if call.data == "button1":
+            users[user_id]['difficult'] = 1
+            users[user_id]['threshold'] = 500000
+        if call.data == "button2":
+            users[user_id]['difficult'] = 2
+            users[user_id]['threshold'] = 100000
+        if call.data == "button3":
+            users[user_id]['difficult'] = 3
+            users[user_id]['threshold'] = 0
+        bot.send_message(user_id, 'Назовите город')
+
+
+def is_city(bot, city, user_id):
+    if city in df_base['city'].tolist():
+        return True
+    else:
+        bot.send_message(user_id, 'Это не город')
+        return False
+
+def last_letter(city):
+    if city.endswith('ый'):
+        return city[-3]
+    elif city[-1] not in ['ь', 'ы', 'й']:
+        return city[-1]
+    else:
+        return city[-2]
+
+def is_played(bot, city, user_id):
+    df = users[user_id]['df']
+    row = df[df['city'] == city].index
+    if df['played'][row].tolist()[0] == 0:
+        return True
+    else:
+        bot.send_message(user_id, 'Этот город уже называли')
+        return False
+
+
+def is_valid(bot, city, user_id):
+    if users[user_id]['key_letter']:
+        if city[0] == users[user_id]['key_letter']:
+            return True
+        else:
+            bot.send_message(user_id, 'Не с той буквы начинается')
+            return False
+    else:
+        return True
+
+
+def mark_as_played(user_id, city):
+    city = city.lower()
+    df = users[user_id]['df']
+    row = df[df['city'] == city].index
+    df['played'][row] = 1
+    users[user_id]['df'] = df
+
+
+def bot_turn(bot, user_id):
+    key_letter = users[user_id]['key_letter']
+    df = users[user_id]['df']
+    threshold = users[user_id]['threshold']
+    sub_df = df[(df['first_letter'] == key_letter) & (df['population'] > threshold) & (df['played'] == 0)]
+    if not sub_df.empty:
+        city = random.choice(sub_df['City'].tolist())
+        mark_as_played(user_id, city)
+        users[user_id]['key_letter'] = last_letter(city)
+        bot.send_message(user_id, city)
+        bot.send_message(user_id, 'Тебе на ' + users[user_id]['key_letter'])
+        
+    else:
+        bot.send_message(user_id, 'Я больше не знаю городов, победа за тобой! Поздравляю! \nНажми /startgame, чтобы начать новую игру')
+        users[user_id]['game_status'] = 0
+
+
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    user_id = message.chat.id
+    city = message.text.lower()
+    if is_city(bot, city, user_id) and is_valid(bot, city, user_id) and is_played(bot, city, user_id):
+        mark_as_played(user_id, city)
+        users[user_id]['key_letter'] = last_letter(city)
+        bot_turn(bot, user_id)
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
